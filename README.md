26ab9d92cb1efb71a5d3d6e8e50acf47389d0733
FahmiShelbyy <133748836+FahmiShelbyy@users.noreply.github.com>
2023-05-31 11:08:58 +0700

    1

---
+++ README.md
@@ -0,0 +1,3 @@
+# aplikasibergerak
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/aplikasibergerak/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aplikasibergerak
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
